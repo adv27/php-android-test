dcc9ceca39184a08daa07b37f3e1901880201a26
anhvd <anhvdse04605@fpt.edu.vn>
2018-10-23 10:56:44 +0700

    make listcar route accept get

--- controllers/listcar.php
+++ controllers/listcar.php
@@ -1,21 +1,21 @@
 <?php
 include 'config.php';
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    function get_cars($db)
-    {
-        $sql = "SELECT * FROM carinfo";
-        $result = $db->query($sql);
-        $cars = array();
-        if ($result->num_rows > 0) {
-            // output data of each row
-            while ($row = $result->fetch_assoc()) {
-               $cars[] = $row;
-            }
-        }
-        return $cars;
-    }
 
-    $cars = get_cars($db);
-    header('Content-Type: application/json');
-    echo json_encode($cars);
+function get_cars($db)
+{
+    $sql = "SELECT * FROM carinfo";
+    $result = $db->query($sql);
+    $cars = array();
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            $cars[] = $row;
+        }
+    }
+    return $cars;
 }
+
+$cars = get_cars($db);
+header('Content-Type: application/json');
+echo json_encode($cars);
+
